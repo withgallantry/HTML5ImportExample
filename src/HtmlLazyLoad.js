d0c7caf11a3f0b8052205cf5b87ca34fad987a35
Matthew Gallantry <matt@wakelet.com>
2015-05-22 23:45:15 +0100

    Refactored to use prototypes instead of multiple iterations

--- src/HtmlLazyLoad.js
+++ src/HtmlLazyLoad.js
@@ -1,65 +1,59 @@
 !(function () {
 
-    this.includeNodes = [];
+    var HTMLIncludeElement = Object.create(HTMLElement.prototype); //Create a copy of the HTMLElement prototype
 
-    this.getTemplateFromUrl = function (url, callback) {
+    HTMLIncludeElement.getIncludeName = function () { //Add function to get the include name. this is scoped to the node.
+        return this.getAttribute('src');
+    };
+
+    HTMLIncludeElement.getIncludeContent = function () { //Add function to get the include content once set
+        return this.includeContent;
+    };
+
+    HTMLIncludeElement.getIncludeRef = function () { //Get the ref of the block to use from include.
+        return this.getAttribute('ref');
+    };
+
+    HTMLIncludeElement.fetchIncludeContent = function () { //Fetch the include content via ajax, fire event upon completion.
+        var includeName = this.getIncludeName();
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
+        xhr.open('GET', includeName, true);
         xhr.onreadystatechange = function (resp) {
             if (xhr.readyState !== 4) return;
             if (xhr.status !== 200) return;
-            callback(url, xhr.response);
+            this.includeContent = xhr.response;
+            var event = new CustomEvent('include-fetched', {includeName: includeName, includeContent: this.includeContent});
+            this.dispatchEvent(event);
         }.bind(this);
         xhr.send();
     };
 
-    this.replaceTemplateDomNodes = function (templateId, content) {
-        var nodes = this.includeNodes;
-        nodes.forEach(function (node) {
-            if (node.getAttribute('src') == templateId) {
-                var ref = node.getAttribute('ref');
-                this.replaceNode(node, content, ref);
-            }
-        }.bind(this));
+    HTMLIncludeElement.replaceWithIncludeBlock = function () {
+        var ref = this.getIncludeRef();
+        var includeContent = this.getIncludeContent();
+        var includeFromContent = new DOMParser().parseFromString(includeContent, "text/html");
+        var nodeContent = includeFromContent.getElementById(ref);
+        //nodeContent.__proto__ = this.__proto__;
+        var event = new CustomEvent('include-complete', {});
+        this.dispatchEvent(event);
+        this.parentNode.replaceChild(nodeContent, this);
+        this.removeEventListener('include-fetched',this.replaceWithIncludeBlock);
     };
 
-    this.replaceNode = function (node, content, ref) {
-        //Refactor this to avoid multiple DOMParser calls, cache, cache, CACHE
-        var templateContent = new DOMParser().parseFromString(content, "text/html");
-        var nodeContent = templateContent.getElementById(ref);
-        node.parentNode.replaceChild(nodeContent, node);
+    this.getIncludeNodes = function () {
+        return Array.prototype.slice.call(document.getElementsByTagName('include'));
     };
 
-    this.getTemplatesToFetch = function (nodes) {
-        function onlyUnique(value, index, self) {
-            return self.indexOf(value) === index;
-        }
-
-        var templateUrls = [];
-        nodes.forEach(function (node) {
-            var templateUrl = node.getAttribute('src');
-            templateUrls.push(templateUrl);
-        }.bind(this));
-
-        return templateUrls.filter(onlyUnique);
-    };
-
-    this.fetchTemplatesAndReplaceHtml = function (templateList) {
-        templateList.forEach(function (templateUrl) {
-            this.getTemplateFromUrl(templateUrl, function(templateId, templateContent) {
-                this.replaceTemplateDomNodes(templateId, templateContent);
-            }.bind(this));
-        }.bind(this));
-    };
-
-    this.replaceNodesWithHtml = function () {
-        this.includeNodes = Array.prototype.slice.call(document.getElementsByTagName('include'));
-        var templateList = this.getTemplatesToFetch(this.includeNodes);
-        this.fetchTemplatesAndReplaceHtml(templateList);
-    };
+    this.upgradeNode = function (node) {
+        node.__proto__ = HTMLIncludeElement; //Replace prototype with custom prototype extended from HTMLElement prototype.
+        node.fetchIncludeContent(); //Start ajax request to get data.
+        node.addEventListener('include-fetched', node.replaceWithIncludeBlock); //Add event listener for when the ajax request has completed. This will replace include node with the correct HTML include
+    }
 
     document.addEventListener('DOMContentLoaded', function () {
-        this.replaceNodesWithHtml();
+        this.getIncludeNodes().forEach(function (node) {
+            this.upgradeNode(node); //Replaces the prototype of the include nodes with prototype created.
+        }.bind(this));
     }.bind(this), false);
 
 
